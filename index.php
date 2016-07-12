ae1ba4e466cc55555ee0d4dc866f2da70b1cf213
Rochelle Lewis <rlewis37@cnm.edu>
2016-07-12 12:51:42 -0600

    added paragraphs, lists

--- index.php
+++ index.php
@@ -6,5 +6,21 @@
 	</head>
 	<body>
 		<h1>Hai World!</h1>
+		<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere, yet peer out window, chatter at birds, lure them to mouth and if it fits, i sits but poop on grasses if it fits, i sits. Gnaw the corn cob love to play with owner's hair tie or meowwww for scratch at the door then walk away favor packaging over toy.</p>
+		<h2>About Me</h2>
+		<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
+		<ul>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ul>
+		<ol>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ol>
+		<div>
+			<a href="https://google.com" target="_blank">Go to Google.com</a>
+		</div>
 	</body>
 </html>
