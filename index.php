8c14dd1606e20183a85b8d112c34eb5bdfb7ff93
Rochelle Lewis <rlewis37@cnm.edu>
2016-07-28 11:02:43 -0600

    create styles: tag, class, element selectors. Add About Me page.

--- index.php
+++ index.php
@@ -15,13 +15,17 @@
 
 		<!-- Begin Header -->
 		<header>
-			<h1>Hai World!</h1>
+			<h1 class="foo-title pretty-title">Hai World!</h1>
 		</header>
 
 		<!-- Start Main Content Here -->
 		<main>
 			<p id="first-paragraph">Howl <strong>uncontrollably</strong> for no <em>reason</em> hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere, yet peer out window, chatter at birds, lure them to mouth and if it fits, i sits but poop on grasses if it fits, i sits. Gnaw the corn cob love to play with owner's hair tie or meowwww for scratch at the door then walk away favor packaging over toy.</p>
 			<h2 class="pretty-title red-text">About Me</h2>
+			<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
+			<h1 class="pretty-title red-text">Antoher HEading</h1>
+			<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
+			<h1 class="pretty-title red-text">Blah....</h1>
 			<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
 
 			<!-- Unordered List -->
