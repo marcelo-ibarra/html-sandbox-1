7e6d26049bd8e526697ebbda30e37a8e746288de
Rochelle Lewis <rlewis37@cnm.edu>
2016-07-28 15:02:40 -0600

    add boxes and css

--- index.php
+++ index.php
@@ -20,13 +20,24 @@
 
 		<!-- Start Main Content Here -->
 		<main>
-			<p id="first-paragraph">Howl <strong>uncontrollably</strong> for no <em>reason</em> hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere, yet peer out window, chatter at birds, lure them to mouth and if it fits, i sits but poop on grasses if it fits, i sits. Gnaw the corn cob love to play with owner's hair tie or meowwww for scratch at the door then walk away favor packaging over toy.</p>
+			<p id="first-paragraph">Howl <strong>uncontrollably</strong> for no <em>reason</em> hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere, yet peer out window, chatter at birds, lure them to mouth and if it fits, i sits but poop on grasses if it fits, i sits. Gnaw the corn cob love to play with owner's hair tie or meowwww for scratch at the door then walk away favor packaging over toy. Howl <strong>uncontrollably</strong> for no <em>reason</em> hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere, yet peer out window, chatter at birds, lure them to mouth and if it fits, i sits but poop on grasses if it fits, i sits. Gnaw the corn cob love to play with owner's hair tie or meowwww for scratch at the door then walk away favor packaging over toy. Howl <strong>uncontrollably</strong> for no <em>reason</em> hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere, yet peer out window, chatter at birds, lure them to mouth and if it fits, i sits but poop on grasses if it fits, i sits. Gnaw the corn cob love to play with owner's hair tie or meowwww for scratch at the door then walk away favor packaging over toy.</p>
+
 			<h2 class="pretty-title red-text">About Me</h2>
 			<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
+
 			<h1 class="pretty-title red-text">Antoher HEading</h1>
-			<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
-			<h1 class="pretty-title red-text">Blah....</h1>
-			<p>Howl uncontrollably for no reason hunt by meowing loudly at 5am next to human slave food dispenser and lick sellotape but refuse to drink water except out of someone's glass. Ignore the squirrels, you'll never catch them anyway leave hair everywhere.</p>
+			<div class="box" id="box-1">
+				box 1
+			</div>
+			<div class="box" id="box-2">
+				box 2
+			</div>
+			<div class="box" id="box-3">
+				box 3
+			</div>
+			<div class="box" id="box-4">
+				box 4
+			</div>
 
 			<!-- Unordered List -->
 			<ul>
